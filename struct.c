81828d81db095fea15359484bb11f78d005590d2
rkhan71 <102491107+rkhan71@users.noreply.github.com>
2022-05-23 20:48:40 +0300

    Update struct.c

--- struct.c
+++ struct.c
@@ -2,6 +2,31 @@
 
 int main(void)
 {
+  typedef struct {
+    char chars
+    char *bytes
+    }
+  key;
   FILE *file = fopen("decodekeynew.csv", "r");
+  char buffer[1024];
+  fgets(buffer, 1024, file);
+  int i = 0;
+  while (fgets(buffer, 1024, file) != NULL) {
+    key[i].chars = buffer[0];
+    int j = 2;
+    char *byte = "";
+    char *bit = buffer[j];
+    while (strcmp(bit, "\n") != 0) {
+      byte = strcat(byte, bit);
+      j++;
+      bit = buffer[j];
+      }
+    key[i].bytes = byte;
+    i++;
+    }
+      
+    
+    
+  
   
   
