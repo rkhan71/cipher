7167bcb4618df4c2c4ab3685880b5993feb0dc1c
rkhan71 <102491107+rkhan71@users.noreply.github.com>
2022-05-24 11:02:01 +0300

    Update struct.c

--- struct.c
+++ struct.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(void)
 {
+  // create struct to put key into
   typedef struct {
     char chars;
     char *bytes;
     }
-  key;
+  keystruct;
+  keystruct key[95];
+
+  // get key from file and fill the struct
   FILE *file = fopen("decodekeynew.csv", "r");
   char buffer[1024];
   fgets(buffer, 1024, file);
   int i = 0;
   while (fgets(buffer, 1024, file) != NULL) {
     key[i].chars = buffer[0];
-    int j = 2;
-    char *byte = "";
-    char *bit = buffer[j];
-    while (strcmp(bit, "\n") != 0) {
-      byte = strcat(byte, bit);
-      j++;
-      bit = buffer[j];
-      }
+    char *byte = malloc(strlen(&buffer[2]));
+    strncpy(byte, &buffer[2], strlen(&buffer[2]) - 2);
     key[i].bytes = byte;
     i++;
     }
+
+  // get message from file? then convert it to cipher text
 }
+
       
     
     
