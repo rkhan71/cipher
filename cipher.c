b0bbcab8195de52a665983cd7be9b5dca089485c
rkhan71 <102491107+rkhan71@users.noreply.github.com>
2022-05-29 11:39:31 +0300

    Create cipher.c

---
+++ cipher.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "functions.h"
+
+int main(int argc, char *argv[])
+{
+    make_struct("decodekeynew.csv");
+    if (strcmp(argv[1], "e") == 0)
+    {
+        FILE *msgfile = fopen(argv[2], "r");
+        encrypt(msgfile, argv[3]);
+    }
+    else if (strcmp(argv[1], "d") == 0)
+    {
+        FILE *encfile = fopen(argv[2], "r");
+        decrypt(encfile, argv[3]);
+    }
+    else
+    {
+        printf("Usage: ./cipher ['e' (encode) / 'd' (decode)] [text file to encode/decode] [name for outfile]\n");
+        return 1;
+    }
+    freemem();
+}
